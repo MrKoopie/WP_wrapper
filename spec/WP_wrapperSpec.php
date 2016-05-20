869a0ca295d95e7f0a3ca0d7ae6ceb05f417dfae
D Koop <mail@danielkoop.me>
2016-05-20 04:40:01 -0400

    Applied fixes from StyleCI

--- spec/WP_wrapperSpec.php
+++ spec/WP_wrapperSpec.php
@@ -3,18 +3,17 @@
 namespace spec\MrKoopie\WP_wrapper;
 
 use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
 
 class WP_wrapperSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    public function it_is_initializable()
     {
         $this->shouldHaveType('MrKoopie\WP_wrapper\WP_wrapper');
     }
 
-    function it_can_get_the_date()
+    public function it_can_get_the_date()
     {
-    	self::print_r('Y',1)
-    		 ->shouldBeEqualTo(print_r('Y', 1));
+        self::print_r('Y', 1)
+             ->shouldBeEqualTo(print_r('Y', 1));
     }
 }
--- src/WP_wrapper.php
+++ src/WP_wrapper.php
@@ -4,25 +4,29 @@ namespace MrKoopie\WP_wrapper;
 
 class WP_wrapper
 {
-	/** 
-	 * Call $function in the root namespace and pass the arguments.
-	 * @param  string $function  The $function name.
-	 * @param  array  $arguments The parameters for $function.
-	 * @return various           Returned data by the function.
-	 */
-	public static function __callStatic($function, $arguments)
-	{
-		return call_user_func_array( '\\'.$function, $arguments);
-	}
+    /**
+     * Call $function in the root namespace and pass the arguments.
+     *
+     * @param string $function  The $function name.
+     * @param array  $arguments The parameters for $function.
+     *
+     * @return various Returned data by the function.
+     */
+    public static function __callStatic($function, $arguments)
+    {
+        return call_user_func_array('\\'.$function, $arguments);
+    }
 
-	/** 
-	 * Call $function in the root namespace and pass the arguments.
-	 * @param  string $function  The $function name.
-	 * @param  array  $arguments The parameters for $function.
-	 * @return various           Returned data by the function.
-	 */
-	public function __call($function, $arguments)
-	{
-		return call_user_func_array( '\\'.$function, $arguments);
-	}
+    /**
+     * Call $function in the root namespace and pass the arguments.
+     *
+     * @param string $function  The $function name.
+     * @param array  $arguments The parameters for $function.
+     *
+     * @return various Returned data by the function.
+     */
+    public function __call($function, $arguments)
+    {
+        return call_user_func_array('\\'.$function, $arguments);
+    }
 }
